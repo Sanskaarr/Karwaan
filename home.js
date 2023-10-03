b49d2680584dc5f42887531da1f1582c068d819d
Sanskar <sanskarjainrocks123s@gmail.com>
2023-10-03 14:25:11 +0530

    Added Shop

---
+++ home.js
@@ -0,0 +1,34 @@
+const hideLoader = function () {
+    $("#loading").hide();
+};
+
+const openNav = function () {
+    $("#right-menu-overlay-close-btn").show();
+    $("#right-home-btn").hide();
+    $("#logo-btn").hide();
+    $("#mySidenav").attr("style", "width:100%");
+    $("#navbarToggler-right-home-menu").attr("style", "display:block");
+    $("#navbarNavDropdown").attr("style", "display:block");
+    $("#mySidenav").addClass("animate__fadeIn");
+};
+
+const closeNav = function () {
+    $("#mySidenav").attr("style", "width:0");
+    $("#mySidenav").removeClass("animate__fadeIn");
+    $("#navbarToggler-right-home-menu").show();
+    $("#right-home-btn").show();
+    $("#logo-btn").show();
+    $("#right-menu-overlay-close-btn").hide();
+};
+$(document).ready(function (e) {
+    setTimeout(hideLoader, 4000);
+
+    $("#right-home-btn,#right-home-btn-burgur").click(function () {
+        openNav();
+    });
+
+    $("#right-menu-overlay-close-btn").click(function () {
+        closeNav();
+    });
+    e.preventDefault();
+});
